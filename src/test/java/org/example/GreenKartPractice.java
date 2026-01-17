643159dc2faedf13b7cbeb634699a391df489081
ap058175 <ap058175@gmail.com>
2026-01-17 18:47:04 +0530

    GreenKart code

--- src/test/java/org/example/GreenKartPractice.java
+++ src/test/java/org/example/GreenKartPractice.java
@@ -11,31 +11,39 @@ import java.util.List;
 
 public class GreenKartPractice {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         Thread.sleep(5000);
-        List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
-        String [] itemsNeeded={"Cucumber","Brocolli","Beetroot","Carrot"};
 
-        List itemsNeededList= Arrays.asList(itemsNeeded);
-        int checked=0;
-        for (int i=0;i<=products.size();i++)
-        {
-            String name=products.get(i).getText().split("-")[0].trim();
+        String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
 
-            if(itemsNeededList.contains(name)){
-
-                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
-                checked++;
-                if(checked==itemsNeeded.length)
-                {
-                    break;
-                }
-
-            }
-        }
+        addItems(driver,itemsNeeded);
 
     }
-}
+
+     public static void addItems(WebDriver driver,String [] itemsNeeded)
+        {
+            List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+            int checked=0;
+            List itemsNeededList = Arrays.asList(itemsNeeded);
+            for (int i=0;i<=products.size();i++)
+            {
+                String name=products.get(i).getText().split("-")[0].trim();
+
+                if(itemsNeededList.contains(name)){
+
+                    driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                    checked++;
+                    if(checked==itemsNeeded.length)
+                    {
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+
+
